91590af9cb849c623a04fd2ef7700da4622b33d8
Friðfinnur M <notandinotandi@gmail.com>
2018-04-01 13:32:48 +0000

    Modified module system, cleanded up bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,73 +1,62 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
 const config = require('./config.json');
+const fs = require('fs');
+const path = require('path');
+
 const module_dir = './modules/';
 const exec_dir = './execs/';
 const util_dir = './utils/';
-var fs = require('fs');
-var path = require('path');
-var modules, execs, utils;
-var commands = {};
+
+const bot = new Discord.Client();
+var commands = [];
 
 function getDirectories(srcpath) {
-    return fs.readdirSync(srcpath).filter((file) => {
-        return fs.statSync(path.join(srcpath, file)).isDirectory();
-    });
+  return fs.readdirSync(srcpath).filter((file) => {
+    return fs.statSync(path.join(srcpath, file)).isDirectory();
+  });
 }
 
 bot.on('ready', () => {
-    console.log('Ready for action!');
-    bot.user.setPresence({
-        game: {
-            name: 'Being awesome'
-        }
-    });
-    
-    modules = getDirectories(module_dir);
-    execs = getDirectories(exec_dir);
-    utils = getDirectories(util_dir);
+  console.log('Ready for action!');
+  bot.user.setPresence({
+    game: {
+      name: 'Being awesome'
+    }
+  });
 
-    // Load modules
-    modules.forEach(cur_module => {
-        try {
-            cur_module = require(module_dir + cur_module);
-        } catch(err) {
-            console.log('Failed to set up module ' + cur_module + ' due to ' + err);
-        }
-        if(module) {
-            if('commands' in module) {
-                for(var j = 0; j < cur_module.commands.length; ++j) {
-                    if(module.commands[j] in module) {
-                        try {
-                            commands[module.commands[j]] =  module[module.commands[j]];
-                        } catch(err) {
-                            console.log('Failed to set up command ' + module.commands[j] + ' due to ' + err);
-                        }
-                    }
-                }
-            }
-        }
-    });
+  const modules = getDirectories(module_dir);
+  const execs = getDirectories(exec_dir);
+  const utils = getDirectories(util_dir);
+
+  // Load modules
+  modules.forEach(cur_module => {
+    try {
+      cur_module = require(module_dir + cur_module);
+      commands.append(cur_module.commands);
+    } catch (err) {
+      console.log('Failed to set up module ' + cur_module + ' due to ' + err);
+    }
+  });
 });
 
-function runCommand(cmd, args) {
-    commands.forEach(cur_cmd => {
-        // TODO
-    });
-}
-
 bot.on('message', msg => {
-    // Good rule of thumb to ignore message
-    // from other bots (no anti-bot stuff pls)
-    if(msg.author.bot) return;
+  // Good rule of thumb to ignore message
+  // from other bots (no anti-bot stuff pls)
+  if (msg.author.bot) return;
 
-    // Ignore messages not starting with prefix
-    // which we set in the config file
-    if(msg.content.indexOf(config.prefix) !== 0) return;
+  // Ignore messages not starting with prefix
+  // which we set in the config file
+  if (msg.content.startsWith(config.prefix)) return;
 
-    // Split message into command and arguments
-    const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
-    const cmd = args.shift.toLowerCase();
+  // Split message into command and arguments
+  const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
+  const cmd_name = args.shift().toLowerCase();
+
+  // See if we have any commands matching whatever we just recieved.
+  // If we do, execute them.
+  commands
+    .filter(cmd => { return cmd_name.match(cmd.test); })
+    .forEach(cmd => { cmd.process(bot, msg, args); });
 });
 
 bot.login(config.token);
--- src/modules/basics.js
+++ src/modules/basics.js
@@ -1,14 +1,15 @@
-exports.commands = [
-    'ping'
-]
-
-exports.ping = {
-    usage: '',
-    description: 'Checks network latency',
-    process: (bot, msg, args) => {
-        msg.channel.send(msg.author + ' pong!');
-        if(args) {
-            msg.channel.send('Note: ping takes no arguments');
-        }
+const ping = {
+  test: /^ping$/,
+  usage: '',
+  description: 'Checks network latency',
+  process: (bot, msg, args) => {
+    msg.channel.send(msg.author + ' pong!');
+    if (args) {
+      msg.channel.send('Note: ping takes no arguments');
     }
-}
+  }
+};
+
+module.exports = {
+  commands: [ping]
+};
